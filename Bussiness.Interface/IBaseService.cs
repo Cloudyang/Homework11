3c71d95b597aa4cafc25b10f379f2718f1e80e28
yy <yy_yeab@hotmail.com>
2017-08-29 21:56:13 +0800

    待续

--- Bussiness.Interface/IBaseService.cs
+++ Bussiness.Interface/IBaseService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Bussiness.Interface.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -32,6 +34,79 @@ namespace Bussiness.Interface
         /// <returns></returns>
         IQueryable<T> Query<T>(Expression<Func<T, bool>> funcWhere) where T : class;
 
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <typeparam name="T">行数据类型</typeparam>
+        /// <typeparam name="S">排序字段类型</typeparam>
+        /// <param name="funcWhere"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="funcOrderby"></param>
+        /// <param name="isAsc"></param>
+        /// <returns></returns>
+        PageResult<T> QueryPage<T, S>(Expression<Func<T, bool>> funcWhere, int pageSize, int pageIndex,
+            Expression<Func<T, S>> funcOrderby, bool isAsc = true) where T : class;
 
+        /// <summary>
+        /// 新增数据，即时Commit
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        T Insert<T>(T t) where T : class;
+
+        /// <summary>
+        /// 新增数据，即时Commit
+        /// 多条Sql 一个连接，事务插入
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tList"></param>
+        /// <returns>返回带主键的集合</returns>
+        IEnumerable<T> Insert<T>(IEnumerable<T> tList) where T : class;
+
+        /// <summary>
+        /// 更新数据,即时Commit
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        void Update<T>(T t) where T : class;
+
+        /// <summary>
+        /// 更新数据，即时Commit
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tList"></param>
+        void Update<T>(IEnumerable<T> tList) where T : class;
+
+        /// <summary>
+        /// 根据主键删除数据，即时Commit
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Id"></param>
+        void Delete<T>(int Id) where T : class;
+
+        /// <summary>
+        /// 删除数据，即时Commit
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        void Delete<T>(T t) where T : class;
+
+        /// <summary>
+        /// 立即保存全部修改
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// 执行Sql 返回集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        IQueryable<T> ExcuteQuery<T>(string sql, SqlParameter[] parameters) where T : class;
+
+        void Excute<T>(string sql, SqlParameter[] parameters) where T : class;
     }
 }
---
+++ Bussiness.Interface/Model/PageResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
+
+namespace Bussiness.Interface.Model
+{
+    public class PageResult<T>
+    {
+        public int TotalCount { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public List<T> DataList { get; set; }
+    }
+}
