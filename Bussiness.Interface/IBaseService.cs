c66cbe8d0b594e7f7d9332a154240cb90a225da3
杨云 <yy_yeab@hotmail.com>
2017-08-29 16:59:23 +0800

    待续

---
+++ Bussiness.Interface/IBaseService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Interface
+{
+    public interface IBaseService : IDisposable
+    {
+        /// <summary>
+        /// 根据Id查询实体
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T Find<T>(int id) where T : class;
+
+        /// <summary>
+        /// 提供对单表的查询
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        IQueryable<T> Set<T>() where T : class;
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="funcWhere"></param>
+        /// <returns></returns>
+        IQueryable<T> Query<T>(Expression<Func<T, bool>> funcWhere) where T : class;
+
+
+    }
+}
